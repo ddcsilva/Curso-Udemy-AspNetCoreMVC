b26fd78ab0154adcbbb6585fcc48a2088c4813f3
Danilo <danilo.silva@msn.com>
2023-04-21 18:58:28 -0300

    Aplicando os atributos Data Annotations

--- DaniloLanches/Models/Categoria.cs
+++ DaniloLanches/Models/Categoria.cs
@@ -1,9 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DaniloLanches.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
+    [Key]
     public int Id { get; set; }
+    
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 100 caracteres")]
+    [Required(ErrorMessage = "Informe o nome da categoria")]
+    [Display(Name = "Nome")]
     public string Nome { get; set; }
+
+    [StringLength(200, MinimumLength = 3, ErrorMessage = "A descrição deve ter entre 3 e 200 caracteres")]
+    [Required(ErrorMessage = "Informe a descrição da categoria")]
+    [Display(Name = "Descrição")]
     public string Descricao { get; set; }
 
     // Propriedade de navegação 1:N - Uma categoria pode ter vários lanches
--- DaniloLanches/Models/Lanche.cs
+++ DaniloLanches/Models/Lanche.cs
@@ -1,15 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DaniloLanches.Models;
 
+[Table("Lanches")]
 public class Lanche
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o nome do lanche")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 100 caracteres")]
     public string Nome { get; set; }
+
+    [Required(ErrorMessage = "Informe a descrição curta do lanche")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "A descrição curta deve ter entre 3 e 100 caracteres")]
     public string DescricaoCurta { get; set; }
+
+    [Required(ErrorMessage = "Informe a descrição detalhada do lanche")]
+    [StringLength(200, MinimumLength = 3, ErrorMessage = "A descrição detalhada deve ter entre 3 e 200 caracteres")]
     public string DescricaoDetalhada { get; set; }
+
+    [Required(ErrorMessage = "Informe o preço do lanche")]
+    [Range(1, 999.99, ErrorMessage = "O preço deve estar entre 1 e 999,99")]
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Preco { get; set; }
+
+    [Display(Name = "Imagem")]
+    [StringLength(200, MinimumLength = 3, ErrorMessage = "O nome da imagem deve ter entre 3 e 200 caracteres")]
     public string ImagemUrl { get; set; }
+
+    [Display(Name = "Imagem Thumbnail")]
+    [StringLength(200, MinimumLength = 3, ErrorMessage = "O nome da imagem thumbnail deve ter entre 3 e 200 caracteres")]
     public string ImagemThumbnailUrl { get; set; }
+
+    [Display(Name = "Preferido?")]
     public bool LanchePreferido { get; set; }
+
+    [Display(Name = "Em estoque?")]
     public bool EmEstoque { get; set; }
 
     // Chave estrangeira
