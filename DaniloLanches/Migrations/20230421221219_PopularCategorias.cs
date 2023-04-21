6752283bc4386df827637ebb0654b467160facd7
Danilo <danilo.silva@msn.com>
2023-04-21 19:15:12 -0300

    Populando a tabela Lanches com dados iniciais

---
+++ DaniloLanches/Migrations/20230421221219_PopularCategorias.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DaniloLanches.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, Descricao) VALUES ('Bebidas', 'Bebidas geladas, cervejas e refrigerantes')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, Descricao) VALUES ('Lanches', 'Lanches variados')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, Descricao) VALUES ('Sobremesas', 'Sobremesas variadas')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, Descricao) VALUES ('Salgados', 'Salgados variados')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
