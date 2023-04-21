7c9fa30842315f42ea5d69a8356cd79fbd733b13
Danilo <danilo.silva@msn.com>
2023-04-21 17:33:58 -0300

    Projeto Inicial

---
+++ DaniloLanches/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DaniloLanches.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
