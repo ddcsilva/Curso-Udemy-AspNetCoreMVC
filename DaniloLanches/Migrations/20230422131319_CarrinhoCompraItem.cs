bb1057114f7dd3a0a4d9f881bd9413773bf6d0b5
Danilo <danilo.silva@msn.com>
2023-04-22 10:15:17 -0300

    Implementando os itens do carrinho de compras

---
+++ DaniloLanches/Migrations/20230422131319_CarrinhoCompraItem.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DaniloLanches.Migrations
+{
+    public partial class CarrinhoCompraItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
