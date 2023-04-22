b5996c1f13c5973bb1bad41e94308a4453ec6229
Danilo <danilo.silva@msn.com>
2023-04-22 10:35:39 -0300

    Adicionar Itens ao Carrinho de Compras

--- DaniloLanches/Models/CarrinhoCompra.cs
+++ DaniloLanches/Models/CarrinhoCompra.cs
@@ -28,5 +28,30 @@ namespace DaniloLanches.Models {
             // Retorna o carrinho de compras
             return new CarrinhoCompra(context) { CarrinhoCompraId = carrinhoCompraId };
         }
+
+        public void AdicionarAoCarrinho(Lanche lanche, int quantidade) {
+            // Define o item do carrinho de compras
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                s => s.Lanche.Id == lanche.Id && s.CarrinhoCompraId == CarrinhoCompraId);
+
+            // Verifica se o item do carrinho de compras existe
+            if (carrinhoCompraItem == null) {
+                // Adiciona o item do carrinho de compras
+                carrinhoCompraItem = new CarrinhoCompraItem {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+
+                // Adiciona o item do carrinho de compras no contexto do banco de dados
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            } else {
+                // Incrementa a quantidade do item do carrinho de compras
+                carrinhoCompraItem.Quantidade++;
+            }
+
+            // Salva as alterações no contexto do banco de dados
+            _context.SaveChanges();
+        }
     }
 }
